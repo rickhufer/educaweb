137ac5023f4a73235c73a7ec6a9c68c831410b87
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2023-12-06 01:45:19 -0500

    feat: add prettier import plugin and env deps

---
+++ api/src/env.ts
@@ -0,0 +1,36 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development'),
+
+    PORT: z
+      .string()
+      .default('3001')
+      .transform((val) => parseInt(val, 10)),
+
+    DATABASE_URL: z.string().url(),
+
+    SECRET_KEY: z.string(),
+  },
+
+  clientPrefix: 'PUBLIC_',
+  client: {
+    // PUBLIC_CLERK_SIGN_IN_URL: z.string().optional(),
+  },
+
+  runtimeEnv: process.env,
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined.
+   * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
+});
